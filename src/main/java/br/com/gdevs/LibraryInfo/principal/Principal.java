fdcd4ea03c01c73a05a7aeeadbb8d1db75712a59
gabriel4502 <gabrielmessias3@hotmail.com>
2024-07-01 19:11:29 -0300

    Criar classe consumo API, e da classe convertedados, para converter dados da requisicao para um json

--- src/main/java/br/com/gdevs/LibraryInfo/principal/Principal.java
+++ src/main/java/br/com/gdevs/LibraryInfo/principal/Principal.java
@@ -1,8 +1,26 @@
 package br.com.gdevs.LibraryInfo.principal;
 
+import br.com.gdevs.LibraryInfo.service.ConsumoApi;
+
+import java.util.Scanner;
+
 public class Principal {
 
+    private ConsumoApi consumoApi = new ConsumoApi();
+        private Scanner scan = new Scanner(System.in);
+        private String BASE_URL = "https://gutendex.com/books&search=";
 
     public void exibeMenu() {
+
+        System.out.println("Nome do autor: ");
+        var autor = scan.nextLine();
+
+
+        var json = consumoApi.obterDados(BASE_URL + autor.replace(" ", "+"));
+
+        System.out.println(json);
+
+
+
     }
 }
